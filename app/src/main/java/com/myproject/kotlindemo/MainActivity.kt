b65513106ff4a3c361b5b741d84d40464568dda8
jemo mgebrishvili <jemo.mgebrishvili@gmail.com>
2016-03-16 14:03:42 +0400

    demo

--- app/src/main/java/com/myproject/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/myproject/kotlindemo/MainActivity.kt
@@ -2,26 +2,34 @@ package com.myproject.kotlindemo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.github.kittinunf.fuel.Fuel
-import com.myproject.kotlindemo.kotlin.http.KotlinHTTP
-import java.util.concurrent.Callable
-import java.util.concurrent.TimeUnit
-import kotlin.concurrent.thread
+import android.util.Log
+import org.jetbrains.anko.async
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 /**
  * Created by Jemo on 3/10/16.
  */
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
         val url = "http://httpbin.org/get"
-        var kotlinHTTP = KotlinHTTP()
-        kotlinHTTP.getResponse(url).toString()
+//        var kotlinHTTP = KotlinHTTP()
+//        kotlinHTTP.getResponse(url).toString()
+//
+//        println("1111 ")
 
-        println("1111 ")
+        async() {
+            val result = URL(url).readText()
+            uiThread {
+                Log.d("Request", result)
+                longToast("Request performed")
+            }
+        }
     }
 
 
